00afdd20bfd3b569ba64158df842b0a4c0d6594e
zloid-rpi <zloid-1@yandex.by>
2020-07-13 12:19:52 +0200

    error: jest and tstng lbry not working correct

--- src/scripts/iCreateElement/__tests__/index.js
+++ src/scripts/iCreateElement/__tests__/index.js
@@ -1,4 +1,7 @@
 import iCreateElement from '../iCreateElement.js'
+// const domTestingLib = require('@testing-library/dom')
+// const { queryHelpers } = domTestingLib
+// import { queryHelpers } from '@testing-library/dom'
  
 describe('iCreateElement()', () => { 
     it('1 + 2 to equal 3', () => {
