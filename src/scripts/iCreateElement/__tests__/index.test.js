b9d05cb4004f67e5b69bbbb183ad1d7842ab2177
zloid-rpi <zloid-1@yandex.by>
2020-07-17 16:19:49 +0200

    upgrade: iCreateElement()

--- src/scripts/iCreateElement/__tests__/index.test.js
+++ src/scripts/iCreateElement/__tests__/index.test.js
@@ -2,48 +2,83 @@ import iCreateElement from '../iCreateElement.js'
 import { screen } from '@testing-library/dom'
 
 describe('iCreateElement()', () => {
+    // basic tests
     it('created new HTMLElement in DOM: <div><i>my custom HTMLElement is ...</i></div>', () => {
-        //test: before
+        // test: before
         const before = screen.queryByText(/^my custom HTMLElement is ...$/i)
         expect(before).toBe(null)
-        //create: DOM-node
+        // create: DOM-node
         iCreateElement({})
-        //test: after
+        // test: after
         const after = screen.getByText(/^my custom HTMLElement is ...$/i)
         expect(after.tagName).toBe('I')
         expect(after.textContent).toBe('my custom HTMLElement is ...')
+        // debug
         screen.debug()
+        console.log(216837)
+        // clear dom
         document.body.innerHTML = ''
     })
     it('created new HTMLElement in DOM: <h3>hello h3</h3>', () => {
-        //test: before
+        // test: before
         const before = screen.queryByText(/^hello h3$/i)
         expect(before).toBe(null)
-        //create: DOM-node
+        // create: DOM-node
         iCreateElement({
             tag: 'h3',
             inner: 'hello h3',
         })
-        //test: after
+        // test: after
         const after = screen.getByText(/^hello h3$/i)
         expect(after.tagName).toBe('H3')
         expect(after.textContent).toBe('hello h3')
+        // debug
         screen.debug()
+        console.log(12987)
+        //clear dom
         document.body.innerHTML = ''
     })
     it('quick inserting any html code in DOM', () => {
-        //test: before
+        // test: before
         const before = screen.queryByText(/^any html code$/i)
         expect(before).toBe(null)
-        //create: DOM-node
+        // create: DOM-node
         iCreateElement({
             inner: '<span class="i-class"><p>any html code</p></span>',
         })
-        //test: after
+        // test: after
         const after = screen.getByText(/^any html code$/i)
         expect(after.textContent).toBe('any html code')
         expect(after.tagName).toBe('P')
+        // debug
         screen.debug()
+        console.log(213978)
+        // clear dom
         document.body.innerHTML = ''
-    })     
+    })
+    // html classes
+    it('default class of created node element must be a defaultClass', () => {
+        // create: DOM-node
+        iCreateElement({ tag: 'div', inner: 'html class' })
+        // test
+        const element = screen.getByText(/^html class$/i)
+        expect(element.className).toBe('defaultClass')
+        // debug
+        screen.debug()
+        console.log(1209834)
+        // clear dom
+        document.body.innerHTML = ''
+    })
+    it('new class of created node element must be a my-best-html-class', () => {
+        // create: DOM-node
+        iCreateElement({ tag: 'div', inner: 'html class', className: 'my-best-html-class' })
+        // test
+        const element = screen.getByText(/^html class$/i)
+        expect(element.className).toBe('my-best-html-class')
+        // debug
+        screen.debug()
+        console.log(978011)        
+        // clear dom
+        document.body.innerHTML = ''
+    })
 })
--- src/scripts/iCreateElement/iCreateElement.js
+++ src/scripts/iCreateElement/iCreateElement.js
@@ -1,30 +1,42 @@
-/**@module src-scripts-iCreateElement */
+/** @module src-scripts-iCreateElement */
 
 /**
  * For simplify creating custom HTMLElements in DOM
  * @function iCreateElement
  * @param {object} props - configure your future DOM element
  * @param {string} props.tag - any html tag
- * @param {string} props.inner - any html; will append to {tag} you set before 
+ * @param {string} props.inner - any html; will append to {props.tag} you set before
+ * @param {string} props.className - html class for {props.tag} element
  * @returns {void} creating custom DOMElement
  * @example
- * // void: <body><div><i>my custom HTMLElement is ...</i></div></body>
+ * // void: <body><div class="defaultClass"><i>my custom HTMLElement is ...</i></div></body>
  * iCreateElement({})
  * @example
- * // void: <body><h3>hello h3</h3></body>
+ * // void: <body><h3 class="defaultClass">hello h3</h3></body>
  * iCreateElement({ tag: 'h3', inner: 'hello h3' })
  * @example
- * // void: <body><span class="i-class"><p>any html code</p></span></body>
+ * // void: <body><div class="defaultClass"><span class="i-class"><p>any html code</p></span></div></body>
  * iCreateElement({
             inner: '<span class="i-class"><p>any html code</p></span>',
         })
+ * @example
+ * // void: <body><div class="defaultClass">html class</div></body>
+ * iCreateElement({ tag: 'div', inner: 'html class' })
+ * @example
+ * // void: <body><div class="my-best-html-class">html class</div></body>
+ * iCreateElement({ tag: 'div', inner: 'html class', className: 'my-best-html-class' })  
  */
 export default function iCreateElement({
     tag = 'div',
     inner = '<i>my custom HTMLElement is ...</i>',
-}) {    
-    //var element = document.createElement(tagName[, options]);
+    className = 'defaultClass',
+}) {
+    // var element = document.createElement(tagName[, options]);
     const customElement = document.createElement(tag)
+    // adding html class
+    customElement.className = className
+    // adding innerHTML
     customElement.innerHTML = inner
+    // append
     document.body.appendChild(customElement)
 }
