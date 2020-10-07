893be0ed9a878e9ada963b1151065b7e68fcebb0
zloid <zloid-1@yandex.by>
2020-10-07 12:39:42 +0200

    refactoring

--- src/utils/myLib.js
+++ src/utils/myLib.js
@@ -1,5 +1,4 @@
 /** @module utils-myLib */
-import iCreateElement from '../utils/iCreateElement'
 
 import store from '../app/store'
 // RTK action
@@ -12,27 +11,14 @@ import {
 // define own lib
 /**
  * library for compose RTK actions
- * @date 2020-09-10
- * @returns {object} window.object
+ * @date 2020-10-7
+ * @returns {Object} window.object
  */
-const rnd = () => Math.floor(Math.random() * 20)
 
 const myLib = {
-    // todo
-    iRnd: () => alert(store.getState().buttonToDisplay.displayData),
-    // todo
-    create: iCreateElement,
-    strTest: () => iCreateElement({}),
-    // todo
-    doRdx: (nmbr = 123123123) => {
-        store.dispatch({
-            type: 'ADD_TODO',
-            text: `myLib.js - ${nmbr.trim()} ~> ${rnd()}`,
-        })
-    },
     buttonToDisplay: {
         btnToDspl: (payload) =>
-            store.dispatch(addButtonValueToScreen(payload.trim())),
+            store.dispatch(addButtonValueToScreen(payload)),
         clearAll: () => store.dispatch(clearAllFromCalcScreen()),
         doEqual: () => store.dispatch(doEqual()),
     },
