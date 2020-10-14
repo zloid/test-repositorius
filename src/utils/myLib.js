5e11a4317e94180d0d096de5ff862eac6a96ed68
zloid <zloid-1@yandex.by>
2020-10-14 15:04:13 +0200

    refactoring

--- src/utils/myLib.js
+++ src/utils/myLib.js
@@ -9,12 +9,11 @@ import {
 } from '../features/buttonToDisplay/buttonToDisplaySlice'
 
 // define own lib
-/**
+/** @class ConnectDispatch
  * library for compose RTK actions
- * @date 2020-10-7
- * @returns {Object} window.object
+ * @returns {Object} collection of all dispathes needs for App
  */
-
+/* 
 const myLib = {
     buttonToDisplay: {
         btnToDspl: (payload) =>
@@ -23,5 +22,25 @@ const myLib = {
         doEqual: () => store.dispatch(doEqual()),
     },
 }
+ */
+// class for creating Singleton
+class ConnectDispatch {
+    buttonToDisplay = {
+        btnToDspl: (payload) => store.dispatch(addButtonValueToScreen(payload)),
+        clearAll: () => store.dispatch(clearAllFromCalcScreen()),
+        doEqual: () => store.dispatch(doEqual()),
+    }
+    constructor() {
+        // reference on the instance, we can check whether or not we’ve already instantiated a ConnectDispatch, and if we have, we won’t create a new one.
+        if (!ConnectDispatch.myLib) {
+            ConnectDispatch.myLib = this
+        }
+
+        return ConnectDispatch.myLib
+    }
+}
+
+const myLib = new ConnectDispatch()
+Object.freeze(myLib)
 
 export default window.myLib = myLib
